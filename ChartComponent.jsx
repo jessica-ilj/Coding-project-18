f9f52b78d5d4633dfe8c69d7f8c0fd3c52625bd6
Jessica Ilok <jilok@usf.edu>
2024-12-08 20:48:48 -0500

    Add reusable ChartComponent

---
+++ ChartComponent.jsx
@@ -0,0 +1,22 @@
+import React, { useRef, useEffect } from 'react';
+import Chart from 'chart.js/auto';
+
+const ChartComponent = ({ type, data, options }) => {
+  const chartRef = useRef(null);
+
+  useEffect(() => {
+    const chartInstance = new Chart(chartRef.current, {
+      type,
+      data,
+      options,
+    });
+
+    return () => {
+      chartInstance.destroy();
+    };
+  }, [type, data, options]);
+
+  return <canvas ref={chartRef}></canvas>;
+};
+
+export default ChartComponent;
